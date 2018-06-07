574916a48dd33eedba3af99830ad028549bd28d2
jamin-hu <38098193+jamin-hu@users.noreply.github.com>
2018-06-07 20:19:36 +0300

    Switched to using Loaders

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
